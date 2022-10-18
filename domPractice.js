94cba96d4593793850845a7db2c9c1a6bf544a77
Yasir Younas <yazyounas@gmail.com>
2022-10-18 13:40:43 +0100

    creae and insert element

---
+++ domPractice.js
@@ -0,0 +1,30 @@
+
+//add h1 in body by using inner html
+document.body.innerHTML = '<h1>This is now the heading of the body element</h1>';
+
+//select and Modify Elements
+document.querySelector('h1').innerHTML = 'Most popular Harry Potter characters';
+
+document.getElementById('fourth').innerHTML = 'Professor Snape';
+document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin';
+
+document.getElementsByTagName('li')[0].innerHTML = 'Dobby';
+
+
+//Style an element with querySelctor
+document.querySelector('body').style.backgroundColor = '#201F2E';
+document.querySelector('.heading').style.fontFamily = 'Roboto';
+
+// make variable to slect parents node nor child node
+let first = document.body.children[0];
+first.innerHTML = 'BROWN BEARS ARE AWESOME!';
+
+first.parentNode.style.backgroundColor = 'beige'; 
+
+
+//Create and Insert Elements
+
+const newAttraction = document.createElement('li');
+newAttraction.id = 'vespa';
+newAttraction.innerHTML = 'Rent a Vespa';
+document.getElementById('italy-attractions').appendChild(newAttraction);

47e178e675c974db2e5b0adaec5b9e2031de7000
Yasir Younas <yazyounas@gmail.com>
2022-10-20 14:58:27 +0100

    added forms project

--- eventListenerFucntions.js
+++ eventListenerFucntions.js
@@ -38,4 +38,16 @@ function showItem() {
 }
 itemFour.addEventListener('mousedown', showItem);
 
+//ball game 
+let ball = document.getElementById('float-circle');
+
+// Write your code below
+function up(){
+  ball.style.bottom = '500px';
+}
+function down(){
+  ball.style.bottom = '50px';
+}
+document.addEventListener('keydown', up);
+document.addEventListener('keyup', down);  
 
